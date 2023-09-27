ea47e9e8bf4d8421501b3451fc0b9869cf3f3194
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-09-27 11:32:53 -0300

    Add files via upload

---
+++ beta/src/main/java/com/mobeats/model/Producto.java
@@ -0,0 +1,139 @@
+package com.mobeats.model;
+
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+import java.util.Date;
+
+
+public class Producto {
+ @Entity
+@Table(name = "productos")
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @Column(name = "updated_by", nullable = false)
+    @LastModifiedBy
+    private String updatedBy;
+
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
+  public long getId() {
+        return id;
+    }
+
+  /**
+   * Sets id.
+   *
+   * @param id the id
+   */
+  public void setId(long id) {
+        this.id = id;
+    }
+
+  /**
+   * Gets first name.
+   *
+   * @return the first name
+   */
+  public String getName() {
+        return name;
+    }
+
+  /**
+   * Sets first name.
+   *
+   * @param name the first name
+   */
+  public void setFirstName(String name) {
+        this.name = name;
+    }
+
+  /**
+   * Gets email.
+   *
+   * @return the email
+   */
+  public String getDescripiton() {
+        return description;
+    }
+
+  /**
+   * Sets email.
+   *
+   * @param description the email
+   */
+  public void setEmail(String description) {
+        this.description = description;
+    }
+
+  /**
+   * Gets updated at.
+   *
+   * @return the updated at
+   */
+  public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+  /**
+   * Sets updated at.
+   *
+   * @param updatedAt the updated at
+   */
+  public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+  /**
+   * Gets updated by.
+   *
+   * @return the updated by
+   */
+  public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+  /**
+   * Sets updated by.
+   *
+   * @param updatedBy the updated by
+   */
+  public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", updatedAt=" + updatedAt +
+                ", updatedby='" + updatedBy + '\'' +
+                '}';
+    }
+
+}
+}
