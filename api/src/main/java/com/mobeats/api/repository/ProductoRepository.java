779187a613c3f1a28401225487cf7fc26e09e243
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-10-06 12:31:01 -0300

    Add files via upload

---
+++ api/src/main/java/com/mobeats/api/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.mobeats.api.repository;
+
+import com.mobeats.api.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {}
