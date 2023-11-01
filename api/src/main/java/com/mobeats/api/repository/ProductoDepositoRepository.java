19592c27150feb1f0b2f9caf86afa4e5d310c3cf
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-11-01 12:09:56 -0300

    Add files via upload

--- api/src/main/java/com/mobeats/api/repository/ProductoDepositoRepository.java
+++ api/src/main/java/com/mobeats/api/repository/ProductoDepositoRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.mobeats.api.model.Producto;
 import com.mobeats.api.model.Deposito;
-
+import java.util.List;
 
 @Repository
 public interface ProductoDepositoRepository extends JpaRepository<ProductoDeposito, Long> {
+    List<ProductoDeposito> findByProductoId(Long productoId);
     ProductoDeposito findByProductoAndDeposito(Producto producto, Deposito deposito);
 }
